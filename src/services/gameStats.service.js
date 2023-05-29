a176e021d1f82aa82e6a4313566a4583e9aa9e63
gng <>
2023-05-29 14:27:43 -0400

    Removed service function that no longer exists

--- src/services/gameStats.service.js
+++ src/services/gameStats.service.js
@@ -2,15 +2,6 @@ import axios from "axios";
 
 const API_URL = 'http://localhost:4000/api';
 
-const update = () => {
-    return axios.get(`${API_URL}/game-stats/update`)
-    .then((res) => {
-        console.log('stats updated')
-
-        return res;
-    });
-};
-
 const get = (league) => {
     return axios.get(`${API_URL}/game-stats/get?league=${league}`)
     .then((res) => {
@@ -19,7 +10,7 @@ const get = (league) => {
 };
 
 const GameStatsService = {
-    get, update
+    get
 };
 
 export default GameStatsService;
